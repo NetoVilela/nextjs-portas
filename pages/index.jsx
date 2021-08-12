30abdf5eca5320257be1aa155683e3dd19e6bea4
NetoVilela <favneto.a.v.n@gmail.com>
2021-08-11 21:51:04 -0300

    primeiro commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../Components/Porta";
+import Presente from "../Components/Presente";
+
+export default function Home() {
+  return (
+    <div>
+        {/* <Presente /> */}
+        <Porta />
+    </div>
+  )
+}
